b4060bbb7bdf8702203796a688e3664d6cdef888
wangbo <wb626@pku.edu.cn>
2020-04-16 22:50:14 +0800

    remove jade support

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -26,7 +25,6 @@ app.use('/webcomponentsjs', express.static(__dirname + '/node_modules/@webcompon
 
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -41,6 +39,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
+  res.type('html');
   res.render('error');
 });
 
